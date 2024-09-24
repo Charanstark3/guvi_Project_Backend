9c2667994b3e85c5ec94e7ae0a5e9c8e4cecd4ba
Charan <130632952+Charanstark3@users.noreply.github.com>
2024-09-24 21:25:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const attendanceRoutes = require('./routes/attendance');
+const leaveRoutes = require('./routes/leave');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Load environment variables
+require('dotenv').config();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/attendance', attendanceRoutes);
+app.use('/api/leave', leaveRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
